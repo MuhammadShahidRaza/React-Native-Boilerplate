21266386b5cf826b18530d133d3689ad9b02a1bf
Ahsan Ali <ahsan.cooperativec@gmail.com>
2025-06-11 20:06:16 +0500

    card fix

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,14 +1,13 @@
-import { View, StyleSheet, StyleProp, TextStyle, Touchable, TouchableOpacity, ViewStyle, SafeAreaView, ImageBackground, Image, ImageStyle } from 'react-native';
+import { View, StyleSheet, TextStyle, ImageBackground, Image, ImageStyle } from 'react-native';
 import { RowComponent } from './Row';
 import { Icon } from './Icon';
 import { VARIABLES } from 'constants/common';
-import { FontSize, FontWeight, ChildrenType } from 'types/index';
+import { FontSize, FontWeight } from 'types/index';
 import { Typography } from './Typography';
-import { onBack } from 'navigation/index';
-import { Photo } from './Photo';
 import { COLORS } from 'utils/colors';
-import { useTranslation } from 'hooks/useTranslation';
 import { IMAGES } from 'constants/assets';
+import { useState } from 'react';
+import { ActivityIndicator } from 'react-native';
 
 
 type Props = {
@@ -40,63 +39,101 @@ export const Card = ({
     rating = ''
 }: Props) => {
 
+    const [loading, setLoading] = useState(false)
+
+    console.log(loading, ' loading')
     if (type === "1") {
-        return (
-            <ImageBackground key={key} style={[styles.container, { marginHorizontal: 20 }, containerStyle]} source={{ uri }}>
+        return <>
+            {loading ? <View style={[styles.container, { marginHorizontal: 20, justifyContent: 'center', alignItems: 'center' }, containerStyle]}>
+                <ActivityIndicator size={"large"} color={COLORS.GREEN} />
+            </View> : null}
+
+            <ImageBackground
+                onLoadStart={() => setLoading(true)}
+                onPartialLoad={() => setLoading(false)}
+                onLoad={() => setLoading(false)}
+                onLoadEnd={() => setLoading(false)}
+                key={key}
+                style={[styles.container, { marginHorizontal: 20 }, containerStyle]} source={{ uri }}>
                 <View style={styles.overLay}>
                     <Typography style={[styles.title, titleStyle]}>{title}</Typography>
                     <Typography style={styles.description}>{description}</Typography>
                 </View>
             </ImageBackground>
-        );
+        </>
+
+
     }
 
     if (type === "2") {
+
+
         return (
-            <View key={key}>
-                <Image style={[styles.container, containerStyle]} source={{ uri }} />
-                <Typography style={[styles.title, titleStyle]}>{title}</Typography>
-            </View>
-        );
+            <>
+                {loading ? <View style={[styles.container, { marginHorizontal: 20, justifyContent: 'center', alignItems: 'center' }, containerStyle]}>
+                    <ActivityIndicator size={"small"} color={COLORS.GREEN} />
+                </View> : null}
+                <View key={key}>
+                    <Image
+                        onLoadStart={() => setLoading(true)}
+                        onLoad={() => setLoading(false)}
+                        onLoadEnd={() => setLoading(false)}
+                        onPartialLoad={() => setLoading(false)}
+                        style={[styles.container, containerStyle]} source={{ uri }} />
+                    <Typography style={[styles.title, titleStyle]}>{title}</Typography>
+                </View>
+            </>
+        )
     }
 
     if (type === "3") {
         return (
-            <View style={[styles.container, styles.typeThreeContainer, containerStyle]} key={key}>
-                <Image style={styles.typeThreeImageStyle} source={{ uri }} />
-                <RowComponent style={{ paddingHorizontal: 20 }}>
-                    <Typography style={[styles.title, titleStyle]}>{title}</Typography>
-                    <RowComponent>
-                        <Image style={styles.ratingIcon} source={IMAGES.RATINGS} />
-                        <Typography style={styles.ratingText}>{rating}</Typography>
+            <>
+                {loading ? <View style={[styles.container, styles.typeThreeContainer, { justifyContent: 'center', alignItems: 'center' }, containerStyle]}>
+                    <ActivityIndicator size={"small"} color={COLORS.GREEN} />
+                </View> : null}
+
+                <View style={[styles.container, styles.typeThreeContainer, containerStyle]} key={key}>
+                    <Image
+                        onLoadStart={() => setLoading(true)}
+                        onLoad={() => setLoading(false)}
+                        onLoadEnd={() => setLoading(false)}
+                        onPartialLoad={() => setLoading(false)}
+
+                        style={styles.typeThreeImageStyle} source={{ uri }} />
+                    <RowComponent style={{ paddingHorizontal: 20 }}>
+                        <Typography style={[styles.title, titleStyle]}>{title}</Typography>
+                        <RowComponent>
+                            <Image style={styles.ratingIcon} source={IMAGES.RATINGS} />
+                            <Typography style={styles.ratingText}>{rating}</Typography>
+                        </RowComponent>
+
                     </RowComponent>
 
-                </RowComponent>
-
-                <RowComponent style={styles.currencyAndLocationContainer}>
-                    <Icon
-                        componentName={VARIABLES.Entypo}
-                        iconName={'location'}
-                        size={15}
-                        onPress={() => { }}
-                        iconStyle={{
-                            marginEnd: 10,
-                            color: COLORS.GREEN,
-                        }}
-                    />
-                    <Typography style={styles.location}>{location}</Typography>
-                </RowComponent>
+                    <RowComponent style={styles.currencyAndLocationContainer}>
+                        <Icon
+                            componentName={VARIABLES.Entypo}
+                            iconName={'location'}
+                            size={15}
+                            onPress={() => { }}
+                            iconStyle={{
+                                marginEnd: 10,
+                                color: COLORS.GREEN,
+                            }}
+                        />
+                        <Typography style={styles.location}>{location}</Typography>
+                    </RowComponent>
 
 
-                <RowComponent style={styles.currencyAndLocationContainer}>
-                    <Image style={styles.currencyNoteImage} source={IMAGES.CURRENCY_NOTE} />
-                    <Typography style={styles.currency}>{currency}</Typography>
-                </RowComponent>
+                    <RowComponent style={styles.currencyAndLocationContainer}>
+                        <Image style={styles.currencyNoteImage} source={IMAGES.CURRENCY_NOTE} />
+                        <Typography style={styles.currency}>{currency}</Typography>
+                    </RowComponent>
 
-            </View>
-        );
+                </View>
+            </>
+        )
     }
-
 
 };
 
