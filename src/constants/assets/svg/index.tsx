1a5399374a421afc405728dcc0ae10199b030873
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-06-13 00:57:00 +0500

    Svg added

--- src/constants/assets/svg/index.tsx
+++ src/constants/assets/svg/index.tsx
@@ -1,8 +1,25 @@
 import LOGO from 'assets/svg/common/logo.svg';
 import FACEBOOK from 'assets/svg/common/facebook.svg';
 import GOOGLE from 'assets/svg/common/google.svg';
+import LIFE_STYLE from 'assets/svg/home/life_style.svg';
+import FOOD from 'assets/svg/home/food.svg';
+import GROCERIES from 'assets/svg/home/groceries.svg';
+import WEARS from 'assets/svg/home/wears.svg';
+import HEALTH from 'assets/svg/home/health.svg';
+import REAL_STATE from 'assets/svg/home/real_state.svg';
+import EVENTS from 'assets/svg/home/events.svg';
+import OTHERS from 'assets/svg/home/others.svg';
+
 export const SVG = {
   LOGO,
   FACEBOOK,
   GOOGLE,
+  LIFE_STYLE,
+  FOOD,
+  GROCERIES,
+  WEARS,
+  HEALTH,
+  REAL_STATE,
+  EVENTS,
+  OTHERS,
 };
