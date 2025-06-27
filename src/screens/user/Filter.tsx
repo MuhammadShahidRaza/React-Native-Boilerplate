b07d53fd1d808687bc37e8f804161516794210f1
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-06-28 00:53:28 +0500

    screen set

--- src/screens/user/Filter.tsx
+++ src/screens/user/Filter.tsx
@@ -1,83 +1,53 @@
-import {
-  Dropdown,
-  Header,
-  Wrapper,
-  Autocomplete,
-  RangeSlider,
-} from 'components/common';
-import {COMMON_TEXT, HOME_TEXT} from 'constants/screens';
-import {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {FontWeight} from 'types/fontTypes';
-import {AddressDetails} from 'utils/location';
+import { SCREENS } from 'constants/index';
+import { Button, Dropdown, Wrapper } from 'components/index';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { AppScreenProps, FontWeight } from 'types/index';
+import { STYLES } from 'utils/commonStyles';
+import { onBack } from 'navigation/index';
 
-export const Filter = () => {
-  const radiusList = [
-    {name: '5 km'},
-    {name: '10 km'},
-    {name: '15 km'},
-    {name: '20 km'},
-    {name: '30 km'},
-    {name: '50 km'},
-    {name: '100 km'},
+export const Filter = ({ route }: AppScreenProps<typeof SCREENS.FILTER>) => {
+  const data = route?.params?.data;
+  const roomList = [
+    { name: '2 Rooms' },
+    { name: '3 Rooms' },
+    { name: '4 Rooms' },
+    { name: '5 Rooms' },
+    { name: '6 Rooms' },
   ];
-  const therapyList = [
-    {name: 'Wet Cupping'},
-    {name: 'Dry Cupping'},
-    {name: 'Hijama Cupping'},
-  ];
-  const specializationList = [
-    {name: 'Hijama Cupping'},
-    {name: 'Dry Hijama Cupping'},
-    {name: 'Wet Hijama Cupping'},
-  ];
-  const [reverseGeocodedAddress, setReverseGeocodedAddress] =
-    useState<AddressDetails | null>(null);
-  const [selectedRadius, setSelectedRadius] = useState('');
-  const [selectedTherapy, setSelectedTherapy] = useState('');
-  const [selectedSpecialization, setSelectedSpecialization] = useState('');
-  const [price, setPrice] = useState([20, 50]);
+  const propertyTypeList = [{ name: 'Renting' }, { name: 'Buying' }, { name: 'Land sales' }];
+  const [selectedRoom, setSelectedRoom] = useState('');
+  const [selectedPropertyType, setSelectedPropertyType] = useState('');
   return (
-    <Wrapper useScrollView>
-      <Header title={COMMON_TEXT.FILTERS} />
-      <View style={{marginHorizontal: 20}}>
-        <Autocomplete
-          containerStyle={styles.spacing}
-          title={COMMON_TEXT.LOCATION}
-          titleStyle={styles.title}
-          setReverseGeocodedAddress={setReverseGeocodedAddress}
-        />
-        <View style={styles.spacing} />
-        <Dropdown
-          titleStyle={styles.title}
-          title={COMMON_TEXT.RADIUS}
-          options={radiusList}
-          selectedValue={selectedRadius}
-          onSelect={setSelectedRadius}
-        />
-        <View style={styles.spacing} />
+    <Wrapper useScrollView={false}>
+      <View style={STYLES.CONTAINER}>
+        {(data?.heading === 'Hotels' || data?.heading === 'Shortlet') && (
+          <Dropdown
+            titleStyle={styles.title}
+            title={'Rooms'}
+            options={roomList}
+            selectedValue={selectedRoom}
+            onSelect={setSelectedRoom}
+          />
+        )}
 
-        <Dropdown
-          titleStyle={styles.title}
-          title={COMMON_TEXT.THERAPY_TYPE}
-          options={therapyList}
-          selectedValue={selectedTherapy}
-          onSelect={setSelectedTherapy}
-        />
+        {data?.heading === 'Real Estate' && (
+          <Dropdown
+            titleStyle={styles.title}
+            title={'Property Type'}
+            options={propertyTypeList}
+            selectedValue={selectedPropertyType}
+            onSelect={setSelectedPropertyType}
+          />
+        )}
+
         <View style={styles.spacing} />
-        <Dropdown
-          titleStyle={styles.title}
-          title={COMMON_TEXT.SPECIALIZATION}
-          options={specializationList}
-          selectedValue={selectedSpecialization}
-          onSelect={setSelectedSpecialization}
-        />
-        <View style={styles.spacing} />
-        <RangeSlider
-          values={price}
-          setValues={setPrice}
-          title={HOME_TEXT.PRICING}
-          titleStyle={styles.title}
+        <Button
+          title={'Apply'}
+          onPress={() => {
+            onBack();
+          }}
+          style={styles.buttonContainer}
         />
       </View>
     </Wrapper>
@@ -88,5 +58,8 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: FontWeight.Bold,
   },
-  spacing: {marginBottom: 10},
+  spacing: { marginBottom: 10 },
+  buttonContainer: {
+    marginTop: 100,
+  },
 });
