188e97693cc984e06ea36bd45232446e83ee0c0d
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-07-25 20:46:57 +0500

    loader issue solved

--- src/components/common/SkeletonLoader.tsx
+++ src/components/common/SkeletonLoader.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 import { ChildrenType } from 'types/index';
@@ -6,15 +5,16 @@ import { ChildrenType } from 'types/index';
 interface SkeletonLoaderProps {
   children: ChildrenType;
   height?: number;
+  isLoading?: boolean
 }
 
-export const SkeletonLoader: React.FC<SkeletonLoaderProps> = ({ children, height }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 500);
-  }, []);
+export const SkeletonLoader: React.FC<SkeletonLoaderProps> = ({ children, height, isLoading = false }) => {
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 500);
+  // }, []);
   return (
     <SkeletonPlaceholder enabled={isLoading}>
       <View
@@ -23,7 +23,7 @@ export const SkeletonLoader: React.FC<SkeletonLoaderProps> = ({ children, height
             ? {
                 borderWidth: 0.8,
                 borderRadius: 20,
-                height
+                height,
               }
             : {}
         }
