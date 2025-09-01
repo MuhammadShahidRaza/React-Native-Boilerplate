21faa00b9a50b42b868374d057fda1d8f49bdcf0
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-01 23:55:32 +0500

    android versions up[dated

--- android/app/src/main/java/com/yourappname/MainActivity.kt
+++ android/app/src/main/java/com/yourappname/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cmolds.xtrafind
+package com.company.yourappname
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "XtraFind"
+  override fun getMainComponentName(): String = "yourappname"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
